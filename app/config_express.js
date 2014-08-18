d02c3864dc82fdc7deb9a65d6804bc8692508f48
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-08-18 12:24:15 +0200

    less && bootstrap

--- app/config_express.js
+++ app/config_express.js
@@ -6,7 +6,10 @@ var app = express();
 var configExpress = function (models) {
     app.engine('jade', jade.__express);
     app.use(morgan());
-    
+
+    app.use('/static/bower_components', express.static(__dirname + '/../bower_components'));
+    app.use('/static/less', express.static(__dirname + '/../less'));
+
     var index = require('../routes/index');
     app.get('/', index(models));
     app.listen(5090);
