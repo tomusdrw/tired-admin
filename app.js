115e3609c7508b2ce7e50b2b7aa9a35a14e9be4b
Tomusdrw <tomusdrw@gmail.com>
2015-01-22 08:41:38 +0100

    Added npm scripts (postinstall and start). Displaying server started message.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
+#!/usr/bin/env node
 
 var config;
 
-require('./app/config_reader')('admin.json').then(function (cfg) {
-    config = cfg;
-    return require('./app/config_mongoose')(config.dbpath);
-}).then(function (mongoose) {
-    return require('./app/config_models')(config.models);
-}).then(function (models) {
-    return require('./app/config_express')(models);
-})
-
-.done();
-
+require('./app/config_reader')('admin.json').then(function(cfg) {
+  config = cfg;
+  return require('./app/config_mongoose')(config.dbpath);
+}).then(function(mongoose) {
+  return require('./app/config_models')(config.models);
+}).then(function(models) {
+  return require('./app/config_express')(models);
+}).done(function() {
+  console.log("Started at localhost:5090");
+});
